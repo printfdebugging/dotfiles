6a108f772d1558e39c007c4342cc97ae5adefe0d
Sahil Gautam <sahil@libreoffice.org>
2024-06-17 21:04:45 +0530

    Nextup Dotfiles.sh

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# why a shell script?
-# because there are a lot of ansible tags, and a lot of tasks,
-# like syncing, file setup, packages refresh etc. this will
-# make life easier. will be just a switch on Args though.
 
-
+# don't touch the inventory file for local changes.
