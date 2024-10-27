be55f0fcde2c09206f572cebeb94f3f7464c87c7
Sahil Gautam <sahil@libreoffice.org>
2024-10-28 00:28:51 +0530

    update

--- roles/emacs/files/init.el
+++ roles/emacs/files/init.el
@@ -18,7 +18,7 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(embark-consult htmlize rainbow-mode yasnippet yaml-mode which-key visual-fill-column vertico tree-sitter-langs sudo-edit org-tidy org-roam org-download org-bullets org-appear orderless ob-async nerd-icons-dired marginalia magit lua-mode lsp-java ligature js2-mode indent-bars harpoon go-mode evil-leader evil-commentary evil-collection doom-themes doom-modeline dired-ranger consult-lsp cmake-project cmake-font-lock all-the-icons-nerd-fonts)))
+   '(clang-format embark-consult htmlize rainbow-mode yasnippet yaml-mode which-key visual-fill-column vertico tree-sitter-langs sudo-edit org-tidy org-roam org-download org-bullets org-appear orderless ob-async nerd-icons-dired marginalia magit lua-mode lsp-java ligature js2-mode indent-bars harpoon go-mode evil-leader evil-commentary evil-collection doom-themes doom-modeline dired-ranger consult-lsp cmake-project cmake-font-lock all-the-icons-nerd-fonts)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
