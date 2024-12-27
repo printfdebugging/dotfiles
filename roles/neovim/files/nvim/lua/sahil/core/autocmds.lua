d0596aaff950dc7b690cf2d5134e7d410c1a40c1
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2024-12-27 09:58:45 +0530

    update

--- roles/neovim/files/nvim/lua/sahil/core/autocmds.lua
+++ roles/neovim/files/nvim/lua/sahil/core/autocmds.lua
@@ -15,6 +15,14 @@ vim.cmd([[
   augroup END
 ]])
 
+vim.cmd([[
+  augroup JavaScriptTabStop
+  autocmd!
+  autocmd FileType c set tabstop=2
+  autocmd FileType c set shiftwidth=2
+  augroup END
+]])
+
 -- vim.cmd([[
 --   augroup MarkdownNoWarnings
 --   autocmd!
