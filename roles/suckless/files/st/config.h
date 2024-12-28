9f2efedcbcd458f5496324ac68b1ece961171b31
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2024-12-28 07:46:37 +0530

    update

--- roles/suckless/files/st/config.h
+++ roles/suckless/files/st/config.h
@@ -129,7 +129,7 @@ const char *colorname[] = {
     [15] = "#d8dee9", /* white   */
 
     /* special colors */
-    [256] = "#181a1f", /* background */
+    [256] = "#000000", /* background */
     [257] = "#d8dee9", /* foreground */
     [258] = "#d8dee9", /* cursor */
 };
