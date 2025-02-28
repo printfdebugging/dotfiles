17f668979885d77c3c552300f517d1ea5bb8dfaa
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-02-28 13:53:56 +0530

    update st and telescope stuff

--- roles/suckless/files/st/config.h
+++ roles/suckless/files/st/config.h
@@ -5,7 +5,7 @@
  */
 static char *font =
     "Iosevka Nerd"
-    "Font:pixelsize=20:style=Regular:antialias=true:autohint=true";
+    "Font:pixelsize=26:style=Regular:antialias=true:autohint=true";
 static int borderpx = 8;
 
 /*
@@ -106,7 +106,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float alpha = 0.8;
+float alpha = 1.0;
 const char *colorname[] = {
 
     /* 8 normal colors */
@@ -129,7 +129,7 @@ const char *colorname[] = {
     [15] = "#d8dee9", /* white   */
 
     /* special colors */
-    [256] = "#000000", /* background */
+    [256] = "#181818", /* background */
     [257] = "#d8dee9", /* foreground */
     [258] = "#51afef", /* cursor */
 };
