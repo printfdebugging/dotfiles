9c8c7501ca92c9d822bec68b37420c7225a0ae53
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-03-16 04:17:44 +0530

    biggest neovim improvements, moved tabline to lualine !!!

--- roles/neovim/files/nvim/lua/sahil/plugins/lualine.lua
+++ roles/neovim/files/nvim/lua/sahil/plugins/lualine.lua
@@ -4,12 +4,30 @@ return {
   config = function()
     require("lualine").setup({
       options = {
-        theme = 'one-dark',
+        -- theme = 'one-dark',
         section_separators = { left = "|", right = "|" },
         component_separators = { left = "|", right = "|" },
+      },
+      sections = {
+        lualine_c = {
+          {
+            'tabs',
+            tab_max_length = 40,
+            max_length = vim.o.columns,
+            mode = 1,
+            path = 0,
+            use_mode_colors = false,
+            tabs_color = {
+              active = 'lualine_c_normal',
+              inactive = 'lualine_c_inactive',
+            },
+            show_modified_status = true,
+            symbols = {
+              modified = '✗',
+            },
+          }
+        }
       }
     })
   end,
 }
-
-
