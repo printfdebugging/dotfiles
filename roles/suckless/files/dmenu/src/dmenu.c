54b40efdedebbaa3a6c419f805c593250a4024b0
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-03-25 21:25:10 +0530

    update dwm/dmenu

--- roles/suckless/files/dmenu/src/dmenu.c
+++ roles/suckless/files/dmenu/src/dmenu.c
@@ -70,11 +70,11 @@ static Clr *scheme[SchemeLast];
 
 static int topbar = 0; /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
-static const char *fonts[]               = {"Iosevka Nerd Font:size=18"};
+static const char *fonts[]               = {"JetBrainsMono Nerd Font:size=13"};
 static const char *prompt                = NULL; /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
     /*     fg         bg       */
-    [SchemeNorm]   = {"#bbbbbb", "#181818"},
+    [SchemeNorm]   = {"#bbbbbb", "#222222"},
     [SchemeSel]    = {"#eeeeee", "#005577"},
     [SchemeOut]    = {"#000000", "#00ffff"},
     [SchemeCursor] = {"#222222", "#bbbbbb"},
--- roles/suckless/files/dwm/src/dwm.c
+++ roles/suckless/files/dwm/src/dwm.c
@@ -405,8 +405,8 @@ static const int          systraypinningfailfirst =
     1;                              /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int   showsystray = 1; /* 0 means no systray */
 static const int   topbar      = 0; /* 0 means bottom bar */
-static const char *fonts[]     = {"Iosevka Nerd Font:size=18"};
-static const char  col_gray1[] = "#181818";
+static const char *fonts[]     = {"JetBrainsMono Nerd Font:size=13"};
+static const char  col_gray1[] = "#222222";
 static const char  col_gray2[] = "#444444";
 static const char  col_gray3[] = "#bbbbbb";
 static const char  col_gray4[] = "#eeeeee";
