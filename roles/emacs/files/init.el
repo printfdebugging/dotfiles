1234491aae831ac776098be2aff2ad9b587111df
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-03-25 18:23:23 +0530

    add emacs

---
+++ roles/emacs/files/init.el
@@ -0,0 +1,19 @@
+(org-babel-load-file "~/.config/emacs/config.org")
+(load "~/.config/emacs/elisp/config.el")
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(all-the-icons-nerd-fonts cmake-font-lock cmake-project consult-lsp dired-ranger doom-modeline doom-themes embark-consult evil-collection
+                              evil-commentary evil-leader exwm-surf git-gutter-fringe go-mode harpoon indent-bars js2-mode ligature
+                              lsp-java lua-mode magit marginalia nano-theme neotree nerd-icons-dired ob-async orderless org-appear
+                              org-bullets org-download org-roam org-tidy rainbow-mode sudo-edit tree-sitter-langs vertico
+                              visual-fill-column yaml-mode yasnippet)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
