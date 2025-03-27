d3786d140f5637f7adcd18cc43be8927a284060c
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-03-28 03:34:37 +0530

    some more changes

--- roles/neovim/files/nvim/lua/sahil/plugins/catppuccin.lua
+++ roles/neovim/files/nvim/lua/sahil/plugins/catppuccin.lua
@@ -1,100 +1,110 @@
 return {
+
   "catppuccin/nvim",
   commit = "18bab5ec4c782cdf7d7525dbe89c60bfa02fc195",
   name = "catppuccin",
   priority = 1000,
+
   config = function()
     require("catppuccin").setup({
-      flavour = "mocha",
+      flavour                = "mocha",
+      transparent_background = true,
+      show_end_of_buffer     = false,
+      term_colors            = true,
+      no_italic              = false,
+      no_bold                = false,
+      no_underline           = false,
+
       background = {
         light = "latte",
-        dark = "mocha",
+        dark  = "mocha",
       },
-      transparent_background = true,
-      show_end_of_buffer = false,
-      term_colors = true,
+
       dim_inactive = {
-        enabled = false,
-        shade = "dark",
+        enabled    = false,
+        shade      = "dark",
         percentage = 0.15,
       },
-      no_italic = false,
-      no_bold = false,
-      no_underline = false,
+
       styles = {
-        comments = {},
+        comments     = {},
         conditionals = {},
-        loops = {},
-        functions = {},
-        keywords = {},
-        strings = {},
-        variables = {},
-        numbers = {},
-        booleans = {},
-        properties = {},
-        types = {},
-        operators = {},
-        misc = {},
+        loops        = {},
+        functions    = {},
+        keywords     = {},
+        strings      = {},
+        variables    = {},
+        numbers      = {},
+        booleans     = {},
+        properties   = {},
+        types        = {},
+        operators    = {},
+        misc         = {},
       },
+
       color_overrides = {
-        all = {},
+        all       = {},
+        frappe    = {},
+        macchiato = {},
+        latte     = {},
+
         mocha = {
           rosewater = "#f5e0dc",
-          flamingo = "#f2cdcd",
-          pink = "#f5c2e7",
-          mauve = "#cba6f7",
-          red = "#f38ba8",
-          maroon = "#eba0ac",
-          peach = "#fab387",
-          yellow = "#ECBE7B",
-          green = "#98be65",
-          teal = "#4db5bd",
-          sky = "#89dceb",
-          sapphire = "#74c7ec",
-          blue = "#51afef",
-          lavender = "#b4befe",
-          text = "#d8dee9",
-          subtext1 = "#bac2de",
-          subtext0 = "#a6adc8",
-          overlay2 = "#9399b2",
-          overlay1 = "#7f849c",
-          overlay0 = "#5B6268",
-          surface2 = "#676E95",
-          surface1 = "#6C7085",
-          surface0 = "#2D3952",
+          flamingo  = "#f2cdcd",
+          pink      = "#f5c2e7",
+          mauve     = "#cba6f7",
+          red       = "#f38ba8",
+          maroon    = "#eba0ac",
+          peach     = "#fab387",
+          yellow    = "#ECBE7B",
+          green     = "#98be65",
+          teal      = "#4db5bd",
+          sky       = "#89dceb",
+          sapphire  = "#74c7ec",
+          blue      = "#51afef",
+          lavender  = "#b4befe",
+          text      = "#d8dee9",
+          subtext1  = "#bac2de",
+          subtext0  = "#a6adc8",
+          overlay2  = "#9399b2",
+          overlay1  = "#7f849c",
+          overlay0  = "#5B6268",
+          surface2  = "#676E95",
+          surface1  = "#6C7085",
+          surface0  = "#2D3952",
         },
-        frappe = {},
-        macchiato = {},
-        latte = {},
       },
       highlight_overrides = {
         mocha = function(mocha)
           return {
-            ["@comment"]                    = { fg = "#5b6268", style = {} },
-            ["@keyword.import"]             = { fg = "#cba6f7", style = {} },
-            ["@keyword.directive"]          = { fg = "#cba6f7", style = {} },
-            ["@keyword.directive.define"]   = { fg = "#f5c2e7", style = {} },
-            ["@constant"]                   = { fg = "#fab387", style = {} },
-            ["@constant.builtin"]           = { fg = "#cba6f7", style = {} },
-            ["@function.call"]              = { fg = "#51afef", style = {} },
-            ["@markup.heading.1.markdown"]  = { fg = "#51afef", style = {} },
-            ["@markup.heading.2.markdown"]  = { fg = "#A9A1E1", style = {} },
-            ["@markup.heading.3.markdown"]  = { fg = "#A9A1E1", style = {} },
-            ["@markup.heading.4.markdown"]  = { fg = "#A9A1E1", style = {} },
-            ["@markup.heading.5.markdown"]  = { fg = "#A9A1E1", style = {} },
-            ["@markup.heading.6.markdown"]  = { fg = "#A9A1E1", style = {} },
-            ["@markup.link.label"]          = { fg = "#51afef", style = {} },
+            ["@comment"]                   = { fg = "#5b6268", style = {} },
+            ["@keyword.import"]            = { fg = "#cba6f7", style = {} },
+            ["@keyword.directive"]         = { fg = "#cba6f7", style = {} },
+            ["@keyword.directive.define"]  = { fg = "#f5c2e7", style = {} },
+            ["@constant"]                  = { fg = "#fab387", style = { "bold" } },
+            ["@constant.macro"]            = { fg = "#cba6f7", style = { "bold" } },
+            ["@constant.builtin"]          = { fg = "#cba6f7", style = {} },
+            ["@function.call"]             = { fg = "#51afef", style = {} },
+            ["@markup.heading.1.markdown"] = { fg = "#51afef", style = {} },
+            ["@markup.heading.2.markdown"] = { fg = "#A9A1E1", style = {} },
+            ["@markup.heading.3.markdown"] = { fg = "#A9A1E1", style = {} },
+            ["@markup.heading.4.markdown"] = { fg = "#A9A1E1", style = {} },
+            ["@markup.heading.5.markdown"] = { fg = "#A9A1E1", style = {} },
+            ["@markup.heading.6.markdown"] = { fg = "#A9A1E1", style = {} },
+            ["@markup.link.label"]         = { fg = "#51afef", style = {} },
           }
         end,
       },
+
       integrations = {
-        cmp = true,
-        gitsigns = true,
-        nvimtree = true,
+        cmp        = true,
+        gitsigns   = true,
+        nvimtree   = true,
         treesitter = true,
-        notify = false,
+        notify     = false,
+
         mini = {
-          enabled = true,
+          enabled           = true,
           indentscope_color = "",
         },
       },
@@ -113,11 +123,11 @@ return {
       highlight CursorLineNr  guibg=#3C435E
       highlight Comment       guifg=#5b6268
 
-      highlight GitSignsAddCul guibg=#3c435e guifg=#98be65
+      highlight GitSignsAddCul    guibg=#3c435e guifg=#98be65
       highlight GitSignsChangeCul guibg=#3c435e guifg=#ecb37b
       highlight GitSignsDeleteCul guibg=#3c435e guifg=#f38ba8
       highlight GitSignsChangedeleteCul guibg=#3c435e guifg=#f38ba8
-      highlight GitSignsTopdeleteCul guibg=#3c435e guifg=#f38ba8
+      highlight GitSignsTopdeleteCul  guibg=#3c435e guifg=#f38ba8
       highlight GitSignsUntrackedCul guibg=#3c435e guifg=#98be65
 
       highlight Visual guibg=#3C435E guifg=none
