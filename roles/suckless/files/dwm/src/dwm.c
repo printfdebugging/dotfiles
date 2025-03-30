f2ecb117258c8a28cce18ac509432f2680f08145
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-03-30 19:39:06 +0530

    font size updates

--- roles/suckless/files/dwm/src/dwm.c
+++ roles/suckless/files/dwm/src/dwm.c
@@ -389,14 +389,14 @@ static xcb_connection_t *xcon;
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx        = 0;  /* border pixel of windows */
+static const unsigned int borderpx        = 2;  /* border pixel of windows */
 static const unsigned int snap            = 4;  /* snap pixel */
 static const int          swallowfloating = 1;  /* 1 means swallow floating windows by default */
 static const int          showbar         = 0;  /* 0 means no bar */
-static const unsigned int gappih          = 0; /* horiz inner gap between windows */
-static const unsigned int gappiv          = 0; /* vert inner gap between windows */
-static const unsigned int gappoh          = 0; /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov          = 0; /* vert outer gap between windows and screen edge */
+static const unsigned int gappih          = 10; /* horiz inner gap between windows */
+static const unsigned int gappiv          = 10; /* vert inner gap between windows */
+static const unsigned int gappoh          = 10; /* horiz outer gap between windows and screen edge */
+static const unsigned int gappov          = 10; /* vert outer gap between windows and screen edge */
 static const int          smartgaps       = 0;  /* 1 means no outer gap when there is only one window */
 static const unsigned int systraypinning  = 0;  /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft   = 0;  /* 0: systray in the right corner, >0: systray on left of status text */
@@ -405,7 +405,7 @@ static const int          systraypinningfailfirst =
     1;                              /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int   showsystray = 1; /* 0 means no systray */
 static const int   topbar      = 0; /* 0 means bottom bar */
-static const char *fonts[]     = {"JetBrainsMono Nerd Font:size=13"};
+static const char *fonts[]     = {"Iosevka Nerd Font:size=14"};
 static const char  col_gray1[] = "#222222";
 static const char  col_gray2[] = "#444444";
 static const char  col_gray3[] = "#bbbbbb";
