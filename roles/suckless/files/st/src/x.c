ea1fbc739518cfcc0e90c426aba09e66bb1753d9
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-04-01 00:24:01 +0530

    a little more transparent

--- roles/suckless/files/st/src/x.c
+++ roles/suckless/files/st/src/x.c
@@ -162,7 +162,7 @@ char *termname = "st-256color";
 unsigned int tabspaces = 8;
 
 /* bg opacity */
-float       alpha       = 0.7;
+float       alpha       = 0.6;
 const char *colorname[] = {
 
     /* 8 normal colors */
