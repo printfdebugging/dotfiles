45db2b801edb17334b188a1be79040779d4267e1
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-04-22 00:02:20 +0530

    tabbed works, added tab numbers

--- roles/suckless/files/tabbed/tabbed.c
+++ roles/suckless/files/tabbed/tabbed.c
@@ -173,7 +173,72 @@ static const char *geometry;
 char *argv0;
 
 /* configuration, allows nested code to access above variables */
-#include "config.h"
+/* appearance */
+static const char  font[]       = "Iosevka Nerd Font:size=14";
+static const char* normbgcolor  = "#222222";
+static const char* normfgcolor  = "#bbbbbb";
+static const char* selbgcolor   = "#005577";
+static const char* selfgcolor   = "#eeeeee";
+static const char* urgbgcolor   = "#111111";
+static const char* urgfgcolor   = "#cc0000";
+static const char  before[]     = "<";
+static const char  after[]      = ">";
+static const char  titletrim[]  = "...";
+static const int   tabwidth     = 200;
+static const Bool  foreground   = True;
+static Bool        urgentswitch = False;
+
+/*
+ * Where to place a new tab when it is opened. When npisrelative is True,
+ * then the current position is changed + newposition. If npisrelative
+ * is False, then newposition is an absolute position.
+ */
+static int  newposition  = 0;
+static Bool npisrelative = False;
+
+#define SETPROP(p)                                                             \
+    {                                                                          \
+        .v = (char*[])                                                         \
+        {                                                                      \
+            "/bin/sh", "-c",                                                   \
+                "prop=\"`xwininfo -children -id $1 | grep '^     0x' |"        \
+                "sed -e's@^ *\\(0x[0-9a-f]*\\) \"\\([^\"]*\\)\".*@\\1 \\2@' |" \
+                "xargs -0 printf %b | dmenu -i -l 10 -vi -p -w $1`\" &&"       \
+                "xprop -id $1 -f $0 8s -set $0 \"$prop\"",                     \
+                p, winid, NULL                                                 \
+        }                                                                      \
+    }
+
+#define MODKEY ControlMask
+static const Key keys[] = {
+    /* modifier             key        function     argument */
+    {MODKEY,             XK_t,     focusonce,   {0}                          },
+    {MODKEY,             XK_t,     spawn,       {0}                          },
+    {MODKEY,             XK_q,     killclient,  {0}                          },
+
+    {MODKEY,             XK_j,     rotate,      {.i = -1}                    },
+    {MODKEY,             XK_k,     rotate,      {.i = +1}                    },
+    {MODKEY | ShiftMask, XK_j,     movetab,     {.i = -1}                    },
+    {MODKEY | ShiftMask, XK_k,     movetab,     {.i = +1}                    },
+    {MODKEY,             XK_Tab,   rotate,      {.i = 0}                     },
+
+    {MODKEY,             XK_grave, spawn,       SETPROP("_TABBED_SELECT_TAB")},
+    {MODKEY,             XK_1,     move,        {.i = 0}                     },
+    {MODKEY,             XK_2,     move,        {.i = 1}                     },
+    {MODKEY,             XK_3,     move,        {.i = 2}                     },
+    {MODKEY,             XK_4,     move,        {.i = 3}                     },
+    {MODKEY,             XK_5,     move,        {.i = 4}                     },
+    {MODKEY,             XK_6,     move,        {.i = 5}                     },
+    {MODKEY,             XK_7,     move,        {.i = 6}                     },
+    {MODKEY,             XK_8,     move,        {.i = 7}                     },
+    {MODKEY,             XK_9,     move,        {.i = 8}                     },
+    {MODKEY,             XK_0,     move,        {.i = 9}                     },
+
+    {MODKEY,             XK_u,     focusurgent, {0}                          },
+    {MODKEY | ShiftMask, XK_u,     toggle,      {.v = (void *) &urgentswitch}},
+
+    {0,                  XK_F11,   fullscreen,  {0}                          },
+};
 
 void
 buttonpress(const XEvent *e)
@@ -326,6 +391,7 @@ drawbar(void)
 	XftColor *col;
 	int c, cc, fc, width;
 	char *name = NULL;
+    char tabtitle[256];
 
 	if (nclients == 0) {
 		dc.x = 0;
@@ -367,9 +433,10 @@ drawbar(void)
 		} else {
 			col = clients[c]->urgent ? dc.urg : dc.norm;
 		}
-		drawtext(clients[c]->name, col);
-		dc.x += dc.w;
-		clients[c]->tabx = dc.x;
+        snprintf(tabtitle, sizeof(tabtitle), "%d: %s", c + 1, clients[c]->name);
+        drawtext(tabtitle, col);
+        dc.x += dc.w;
+        clients[c]->tabx = dc.x;
 	}
 	XCopyArea(dpy, dc.drawable, win, dc.gc, 0, 0, ww, bh, 0, 0);
 	XSync(dpy, False);
