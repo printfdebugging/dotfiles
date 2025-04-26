e1fe74557024d71b6b80131d520b5278252c54c9
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-04-26 05:30:20 +0530

    solarized!

--- roles/emacs/files/elisp/face-customizations.el
+++ roles/emacs/files/elisp/face-customizations.el
@@ -1,43 +1,5 @@
-(defun my-customize-faces ()
   ;; most of the faces should be of regular weight
-  (dolist (face '(tree-sitter-hl-face:variable
-                  tree-sitter-hl-face:punctuation.delimiter
-                  tree-sitter-hl-face:doc
-                  tree-sitter-hl-face:tag
-                  tree-sitter-hl-face:type
-                  tree-sitter-hl-face:label
-                  tree-sitter-hl-face:escape
-                  tree-sitter-hl-face:method
-                  tree-sitter-hl-face:number
-                  tree-sitter-hl-face:string
-                  tree-sitter-hl-face:comment
-                  tree-sitter-hl-face:keyword
-                  tree-sitter-hl-face:constant
-                  tree-sitter-hl-face:embedded
-                  tree-sitter-hl-face:function
-                  tree-sitter-hl-face:operator
-                  tree-sitter-hl-face:property
-                  tree-sitter-hl-face:attribute
-                  tree-sitter-hl-face:type.super
-                  tree-sitter-hl-face:constructor
-                  tree-sitter-hl-face:method.call
-                  tree-sitter-hl-face:punctuation
-                  tree-sitter-hl-face:type.builtin
-                  tree-sitter-hl-face:function.call
-                  tree-sitter-hl-face:type.argument
-                  tree-sitter-hl-face:function.macro
-                  tree-sitter-hl-face:string.special
-                  tree-sitter-hl-face:type.parameter
-                  tree-sitter-hl-face:constant.builtin
-                  tree-sitter-hl-face:function.builtin
-                  tree-sitter-hl-face:function.special
-                  tree-sitter-hl-face:variable.builtin
-                  tree-sitter-hl-face:variable.special
-                  tree-sitter-hl-face:variable.parameter
-                  tree-sitter-hl-face:property.definition
-                  tree-sitter-hl-face:punctuation.bracket
-                  tree-sitter-hl-face:punctuation.special
-                  font-lock-doc-face
+  (dolist (face '(font-lock-doc-face
                   font-lock-string-face
                   font-lock-regexp-face
                   font-lock-type-face
@@ -47,6 +9,7 @@
                   font-lock-number-face
                   font-lock-bracket-face
                   font-lock-comment-face
+                  button
                   font-lock-keyword-face
                   font-lock-warning-face
                   font-lock-operator-face
@@ -67,86 +30,11 @@
                   font-lock-regexp-grouping-construct))
     (set-face-attribute face nil
                         :weight 'normal
-                        :height 160
+                        :height 135
+                        :foreground "#839496"
                         :slant 'normal
                         :underline nil))
 
-  ;; some faces should be bold
-  (dolist (face '(tree-sitter-hl-face:method
-                  tree-sitter-hl-face:constant
-                  tree-sitter-hl-face:embedded
-                  tree-sitter-hl-face:function
-                  tree-sitter-hl-face:property
-                  tree-sitter-hl-face:type.super
-                  tree-sitter-hl-face:constructor
-                  tree-sitter-hl-face:method.call
-                  tree-sitter-hl-face:function.call
-                  tree-sitter-hl-face:function.macro
-                  tree-sitter-hl-face:constant.builtin
-                  tree-sitter-hl-face:function.builtin
-                  tree-sitter-hl-face:function.special
-                  tree-sitter-hl-face:variable.builtin))
-    (set-face-attribute face nil
-                        :weight 'bold
-                        :slant 'normal
-                        :height 160
-                        :underline nil))
-
-  (dolist (face '(tree-sitter-hl-face:type.builtin
-                  tree-sitter-hl-face:function.builtin
-                  tree-sitter-hl-face:constructor
-                  tree-sitter-hl-face:variable.builtin))
-    (set-face-attribute face nil
-                        :weight 'normal
-                        :foreground "#ECBE7B"
-                        :slant 'normal
-                        :underline nil))
-
-  ;; bold constants
-  (dolist (face '(tree-sitter-hl-face:constant.builtin
-                  font-lock-constant-face
-                  tree-sitter-hl-face:constant))
-    (set-face-attribute face nil
-                        :weight 'bold
-                        :foreground "#fab387"
-                        :slant 'normal
-                        :underline nil))
-
-  (set-face-attribute 'tree-sitter-hl-face:variable.parameter nil
-                      :weight 'normal
-                      :foreground "#eba0ac"
-                      :slant 'normal
-                      :underline nil)
-
-  (set-face-attribute 'tree-sitter-hl-face:property.definition nil
-                      :weight 'normal
-                      :foreground "#b4befe"
-                      :slant 'normal
-                      :underline nil)
-
-  (dolist (face '(tree-sitter-hl-face:function
-                  tree-sitter-hl-face:function.call
-                  tree-sitter-hl-face:function.macro
-                  font-lock-function-name-face
-                  font-lock-function-call-face
-                  tree-sitter-hl-face:function.builtin
-                  tree-sitter-hl-face:function.special
-                  tree-sitter-hl-face:method.call))
-    (set-face-attribute face nil
-                        :weight 'normal
-                        :foreground "#51afef"
-                        :slant 'normal
-                        :underline nil))
-
-  (dolist (face '(flymake-warning
-                  flymake-note
-                  flymake-error))
-    (set-face-attribute face nil
-                        :weight 'normal
-                        :slant 'normal
-                        :underline nil)))
-
-(add-hook 'lsp-after-initialize-hook 'my-customize-faces)
 
 (set-face-attribute 'button nil
                     :weight 'normal
@@ -170,33 +58,40 @@
                 variable-pitch))
   (set-face-attribute face nil
                       :family "Iosevka Nerd Font"
-                      :height 160
+                      :height 135
                       :weight 'Regular
-                      :foreground "#d8dee9"))
+                      :foreground "#839496"
+                      :background "#001f27"))
 
 ;; org mode heading and source block faces
 (set-face-attribute 'org-document-title nil
                     :family "Iosevka Nerd Font"
-                    :height 160
+                    :height 135
                     :weight 'Bold
                     :foreground "#bbc2cf")
 
 (set-face-attribute 'org-level-1 nil
                     :family "Iosevka Nerd Font"
-                    :height 160
+                    :height 135
                     :weight 'Bold)
 
 (set-face-attribute 'org-level-2 nil
                     :family "Iosevka Nerd Font"
-                    :height 160
+                    :height 135
                     :foreground "#A9A1E1"
                     :weight 'Bold)
 
 (set-face-attribute 'org-level-3 nil
                     :family "Iosevka Nerd Font"
-                    :height 160
+                    :height 135
                     :foreground "#A9A1E1"
                     :weight 'Bold)
 
 (set-face-attribute 'org-block-begin-line nil
                     :foreground "#23272e")
+
+(set-face-attribute 'mode-line nil
+                    :background "#002b36")
+
+(set-face-attribute 'mode-line-inactive nil
+                    :background "#002b36")
--- roles/neovim/files/nvim/init.lua
+++ roles/neovim/files/nvim/init.lua
@@ -2,3 +2,15 @@ require("sahil.core")
 require("sahil.lazy")
 require("sahil.core.keymaps")
 require("sahil.core.autocmds")
+
+local orig_notify = vim.notify
+
+local filter_notify = function(text, level, opts)
+  if type(text) == "string" and string.find(text, ":checkhealth", 1, true) then
+    return
+  end
+
+  orig_notify(text, level, opts)
+end
+
+vim.notify = filter_notify
