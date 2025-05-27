f5451297d32445de9a18f868c47cea0df3256411
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-05-27 22:14:43 +0530

    some git repo changes

--- roles/neovim/files/nvim/init.lua
+++ roles/neovim/files/nvim/init.lua
@@ -1,16 +1,7 @@
+vim.deprecate = function() end
+
 require("sahil.core")
 require("sahil.lazy")
 require("sahil.core.keymaps")
 require("sahil.core.autocmds")
 
-local orig_notify = vim.notify
-
-local filter_notify = function(text, level, opts)
-  if type(text) == "string" and string.find(text, ":checkhealth", 1, true) then
-    return
-  end
-
-  orig_notify(text, level, opts)
-end
-
-vim.notify = filter_notify
