3239f1c1dfd79afb2f2e3d526a098187f13b6810
Sahil Gautam <sahil.gautam.extern@allotropia.de>
2025-06-08 22:24:01 +0530

    remove thunderbird from .xinitrc i.e. no more noise (allow ibl for md)

--- roles/neovim/files/nvim/lua/sahil/plugins/ibl.lua
+++ roles/neovim/files/nvim/lua/sahil/plugins/ibl.lua
@@ -5,7 +5,7 @@ return {
   config = function()
     require("ibl").setup({
       indent = { char = "│" },
-      exclude = { filetypes = { "markdown", "conf" } },
+      exclude = { filetypes = {"conf" } },
       whitespace = {
         remove_blankline_trail = false,
       },
