f1de5bc616cd46c68110044dac6874d9aaf9305a
Sahil Gautam <sahil.gautam@collabora.com>
2025-08-24 08:12:41 +0530

    format on write in neovim

---
+++ roles/neovim/files/nvim/lua/sahil/plugins/formatter.lua
@@ -0,0 +1,32 @@
+return {
+	"stevearc/conform.nvim",
+	opts = {},
+	config = function()
+		require("conform").setup({
+			formatters_by_ft = {
+				lua = { "stylua" },
+				c = { "clang-format" },
+				python = { "isort", "black" },
+			},
+			-- format_on_save = {
+			-- 	lsp_format = "fallback",
+			-- 	timeout_ms = 500,
+			-- },
+		})
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			pattern = "*",
+			callback = function(args)
+				current_file_path = vim.fn.expand("%:p")
+				excluded_strings = {
+					"online",
+				}
+				for _, str in ipairs(excluded_strings) do
+					if string.match(current_file_path, str) then
+						return
+					end
+				end
+				require("conform").format({ bufnr = args.buf })
+			end,
+		})
+	end,
+}
--- roles/neovim/files/nvim/lua/sahil/plugins/mason-lspconfig.lua
+++ roles/neovim/files/nvim/lua/sahil/plugins/mason-lspconfig.lua
@@ -3,7 +3,6 @@ return {
 	dependencies = {
 		"williamboman/mason.nvim",
 		"neovim/nvim-lspconfig",
-		"jay-babu/mason-null-ls.nvim",
 	},
 	config = function()
 		require("mason").setup()
@@ -22,25 +21,6 @@ return {
 				"cssls",
 				"emmet_ls",
 			},
-			-- handlers = {
-			-- 	-- this solves the issue that I faced while working on SFML i.e.
-			-- 	-- neovim including headers which are not required
-			-- },
-		})
-
-		require("mason-null-ls").setup({
-			ensure_installed = {
-				"prettier", -- ts/js formatter
-				"gofmt",
-				"stylua", -- lua formatter
-				"eslint_d", -- ts/js linter
-				-- "mypy",
-				"ruff",
-				"black",
-				"clang-format",
-				"checkmake",
-			},
-			automatic_installation = true,
 		})
 	end,
 }
